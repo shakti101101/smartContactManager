ad09fd73566e4a1b2278417cb0d6e53af34cd298
shakti101101 <154258548+shakti101101@users.noreply.github.com>
2023-12-30 23:26:25 +0530

    change for war file create

--- src/main/java/com/smart/SmartContactManagerApplication.java
+++ src/main/java/com/smart/SmartContactManagerApplication.java
@@ -2,14 +2,21 @@ package com.smart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmartContactManagerApplication {
+public class SmartContactManagerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContactManagerApplication.class, args);
 	
 	System.out.println("project running smartContactManager");
 	}
-
+	
+	/*
+	 * @Override protected SpringApplicationBuilder
+	 * configure(SpringApplicationBuilder builder) { return
+	 * builder.sources(Application.class); }
+	 */
 }
